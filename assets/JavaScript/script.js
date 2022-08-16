36f3ba96b7be9de2c853d14e72b674b6867c399e
Fabio Aurora <fabioauroradev@gmail.com>
2022-08-16 23:24:00 +0100

    completed landing page
    will host it in gitHub pages to test

---
+++ assets/JavaScript/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 10500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
